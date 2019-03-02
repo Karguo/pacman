3776af0b8c53283ca72109030948aed85ebe07dd
Karen Kuek <karenkk75@gmail.com>
2019-03-02 18:25:24 +1100

    move north and south

--- spec/pacman/puck_spec.rb
+++ spec/pacman/puck_spec.rb
@@ -6,22 +6,42 @@ RSpec.describe Pacman::Puck do
 
   it "moves 2 spaces east" do
     2.times { subject.move_east }
-    expect(subject.position).to eq(2)
+    expect(subject.east).to eq(2)
   end
 
   it "moves 3 spaces east" do
     3.times { subject.move_east }
-    expect(subject.position).to eq(3)
+    expect(subject.east).to eq(3)
   end
 
   it "moves 2 spaces west" do
     2.times { subject.move_west }
-    expect(subject.position).to eq(-2)
+    expect(subject.east).to eq(-2)
   end
 
   it "moves 3 spaces west" do
     3.times { subject.move_west }
-    expect(subject.position).to eq(-3)
+    expect(subject.east).to eq(-3)
+  end
+
+  it "moves 2 spaces north" do
+    2.times { subject.move_north }
+    expect(subject.north).to eq(2)
+  end
+
+  it "moves 3 spaces north" do
+    3.times { subject.move_north }
+    expect(subject.north).to eq(3)
+  end
+
+  it "moves 2 spaces south" do
+    2.times { subject.move_south }
+    expect(subject.north).to eq(-2)
+  end
+
+  it "moves 3 spaces south" do
+    3.times { subject.move_south }
+    expect(subject.north).to eq(-3)
   end
 
 end
